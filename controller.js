3fff2fd8b6a8bdcf07054227d4cbe0f2571102c4
fernando.gimenezcacho <fernando.gimenezcacho.ext@telefonica.com>
2014-04-11 14:50:14 +0200

    avances

--- controller.js
+++ controller.js
@@ -7,13 +7,14 @@ App.controller('MundialCtrl', function($scope, $http)
 		{
 		  $scope.mundial = res.data;                
 		});
+
 	$scope.findEquipo = function(equipo) {
 		for (var i=0; i<$scope.mundial.equipos.length; i++)
 	    	if ($scope.mundial.equipos[i].id == equipo)
 	    		return $scope.mundial.equipos[i].nombre;
 	};
 
-	
+
 });
 
 App.controller('ResultadoCtrl', function($scope, $http) 
@@ -21,6 +22,16 @@ App.controller('ResultadoCtrl', function($scope, $http)
 	$http.get('resultado.json')
 		.then(function(res)
 		{
-		  $scope.resultado = res.data;                
+		  $scope.partidos = res.data;
+		  console.log($scope.partidos);
 		});
-	});
+	
+
+	$scope.test = function() {
+		//return $scope.partidos;
+		//for (var i=0; i<$scope.partidos.resultados.length; i++)	
+		    return $scope.partidos.resultados;//$scope.partidos.resultados[i].id;
+
+	};
+	
+});
